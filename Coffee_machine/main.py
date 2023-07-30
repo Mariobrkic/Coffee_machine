2ed3b69f1291ccfb307a8fe510c592ded4a106a7
Mario Brkić <51093050+Mariobrkic@users.noreply.github.com>
2023-07-30 18:03:40 +0200

    Update main.py

--- Coffee_machine/main.py
+++ Coffee_machine/main.py
@@ -44,10 +44,10 @@ def is_resource_sufficient(order_ingredients):
 def process_coins():
     """Returns the total calculated from coins inserted."""
     print("Please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.25
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
     return total
 
 
